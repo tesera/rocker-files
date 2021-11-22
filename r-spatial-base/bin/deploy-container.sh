8043134c958d59ab6b2fc693240845720531c2cb
mkieser <michael.kieser@tesera.com>
2021-11-22 12:12:17 -0400

    adds AWSCLI-v2, container version 4.1.1

--- r-spatial-base/bin/deploy-container.sh
+++ r-spatial-base/bin/deploy-container.sh
@@ -7,8 +7,8 @@
 CONTAINER="tesera/r-spatial-base"
 ACCOUNT="940145619248"
 REGION="us-west-2"
-ECR_IMAGE_NAME="${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com/${CONTAINER}:4.1.0"
-DOCKERHUB_IMAGE_NAME="tesera/r-spatial-base:4.1.0"
+ECR_IMAGE_NAME="${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com/${CONTAINER}:4.1.1"
+# DOCKERHUB_IMAGE_NAME="tesera/r-spatial-base:4.1.0"
 
 # Login to AWS ECR
 aws ecr get-login-password --region us-west-2 --profile hris-development
@@ -20,10 +20,10 @@ if [[ -z `aws ecr describe-repositories | grep "$CONTAINER"` ]]; then
 fi
 
 # Push to AWS ECR
-docker tag "${CONTAINER}:4.1.0" $ECR_IMAGE_NAME
+docker tag "${CONTAINER}:4.1.1" $ECR_IMAGE_NAME
 docker push $ECR_IMAGE_NAME
 docker rmi $ECR_IMAGE_NAME
 
-# Push to dockerhub
-docker tag "${CONTAINER}:4.1.0" $DOCKERHUB_IMAGE_NAME
-docker push $DOCKERHUB_IMAGE_NAME
+# # Push to dockerhub
+# docker tag "${CONTAINER}:4.1.0" $DOCKERHUB_IMAGE_NAME
+# docker push $DOCKERHUB_IMAGE_NAME
