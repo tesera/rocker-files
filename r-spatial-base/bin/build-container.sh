83bb1ed5ab81950462f870a27b6dcc86bb309a6e
mkieser <michael.kieser@tesera.com>
2024-07-30 12:32:10 -0300

    updates bin scripts

--- r-spatial-base/bin/build-container.sh
+++ r-spatial-base/bin/build-container.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
 FORCE_REBUILD=false
-CONTAINER="tesera/r-spatial-base:4.4.1"
+REGION="us-west-2"
+ACCOUNT="712635442529"
+CONTAINER="tesera/r-spatial-base"
+BUILD_PROFILE="hris-workflows"
+ECR_IMAGE_NAME=${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com/${CONTAINER}
+echo $ECR_IMAGE_NAME
 
-aws ecr get-login-password --region us-west-2 --profile hris-development | docker login --username AWS --password-stdin 940145619248.dkr.ecr.us-west-2.amazonaws.com
+FORCE_REBUILD=FALSE
+
+image_tag="dev"
+
+# check for tag 
+if [ ! -z ${TAG+x} ] ; then
+  echo "tag specified"
+  image_tag=$TAG
+fi
+
+aws ecr get-login-password --region $REGION --profile $BUILD_PROFILE | docker login --username AWS --password-stdin $ACCOUNT.dkr.ecr.$REGION.amazonaws.com
 
 usage() { printf "Usage: $0 [-f]\n  -f  Force docker re-build from nothing. (uses docker --no-cache)\n" 1>&2; exit 1; }
 
 while getopts ":fh" opt; do
   case $opt in
     f)
-      FORCE_REBUILD=true
+      FORCE_REBUILD=TRUE
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -25,7 +40,7 @@ while getopts ":fh" opt; do
 done
 
 if $FORCE_REBUILD; then
-    docker build --no-cache -t $CONTAINER .
+    docker build --no-cache -t $ECR_IMAGE_NAME:$image_tag .
 else
-    docker build -t $CONTAINER .
+    docker build -t $ECR_IMAGE_NAME:$image_tag .
 fi
--- r-spatial-base/bin/deploy-container.sh
+++ r-spatial-base/bin/deploy-container.sh
@@ -4,26 +4,35 @@
 #
 # This is a simple helper script to deploy a container to amazon ecr.
 
-CONTAINER="tesera/r-spatial-base"
-ACCOUNT="940145619248"
+echo "deploy container"
+
+ACCOUNT="712635442529"
 REGION="us-west-2"
-ECR_IMAGE_NAME="${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com/${CONTAINER}:4.4.1"
-# DOCKERHUB_IMAGE_NAME="tesera/r-spatial-base:4.1.0"
+CONTAINER="tesera/r-spatial-base"
+BUILD_PROFILE="hris-workflows"
+ECR_IMAGE_NAME="${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com/${CONTAINER}"
+echo $ECR_IMAGE_NAME
 
-# Login to AWS ECR
-aws ecr get-login-password --region us-west-2 --profile hris-development
+image_tag="dev"
 
-# Create a repository for $CONTAINER if not exists
-if [[ -z `aws ecr describe-repositories | grep "$CONTAINER"` ]]; then
-  echo "Creating Repository"
-  REPO_CREATED=`aws ecr create-repository --repository-name "$CONTAINER"`
+# check for tag 
+if [ ! -z ${TAG+x} ] ; then
+  echo "tag specified"
+  image_tag=$TAG
 fi
 
-# Push to AWS ECR
-docker tag "${CONTAINER}:4.4.1" $ECR_IMAGE_NAME
-docker push $ECR_IMAGE_NAME
-docker rmi $ECR_IMAGE_NAME
+# Login to AWS ECR
+aws ecr get-login-password --region $REGION --profile $BUILD_PROFILE | docker login --username AWS --password-stdin $ACCOUNT.dkr.ecr.$REGION.amazonaws.com
 
-# # Push to dockerhub
-# docker tag "${CONTAINER}:4.1.0" $DOCKERHUB_IMAGE_NAME
-# docker push $DOCKERHUB_IMAGE_NAME
+
+# Create a repository for $CONTAINER if not exists
+echo $CONTAINER
+echo $(aws ecr describe-repositories --profile $BUILD_PROFILE | grep "$CONTAINER")
+
+if [[ -z `aws ecr describe-repositories --profile $BUILD_PROFILE | grep "$CONTAINER"` ]]; then
+  echo "Creating Repository"
+  REPO_CREATED=$(aws ecr create-repository --repository-name $CONTAINER --profile $BUILD_PROFILE)
+fi
+
+# Push image to AWS ECR
+docker push ${ECR_IMAGE_NAME}:$image_tag
